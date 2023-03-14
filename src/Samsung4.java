1750117e41e1c87a6a9807c3291869cf652980d9
KWH9788 <dngus9788@gmail.com>
2023-03-14 16:21:11 +0900

    [Gold IV] 경사로 Time: 148ms, Memory: 14916KB

--- src/Samsung4.java
+++ src/Samsung4.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-import javax.swing.text.AsyncBoxView;
-
 /*
     answer = 지나갈 수 있는 길 개수
     N = 행, 열 개수
